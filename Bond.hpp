506b48929045a701a352b7396ef59fde494b1e6f
Anubhab Haldar <ahaldar@bu.edu>
2020-11-04 12:34:39 -0500

    Refactoring continues.

--- Bond.hpp
+++ Bond.hpp
@@ -3,10 +3,4 @@
 template <class IntegerType> struct Bond {
   IntegerType s1;
   IntegerType s2;
-  void invert_spins() {
-    s1 *= -1;
-    s2 *= -1;
-  }
-
-  inline bool are_spins_parallel() { return s1 & s2; }
 };
